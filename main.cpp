cd4321c51b725279ced2e4cba7cf5fb624fd0a5d
Samuel Falk <sammey1995@gmail.com>
2020-12-08 09:04:58 +0000

    back to normin

--- main.cpp
+++ main.cpp
@@ -1,63 +1,62 @@
 //  ====================
-     #include <iostream>
-    using namespace std;
+#include <iostream>
+using namespace std;
 //  ====================
 
 //  =====================
 //  Constant Declarations
 //  ==================================
-    const int MIN_RANDOM_NUMBER =   0;
-    const int MAX_RANDOM_NUMBER = 100;
-    const int COLUMNS_IN_ARRAY  =  10;
-    const int ROWS_IN_ARRAY     =  10;
+const int MIN_RANDOM_NUMBER = 0;
+const int MAX_RANDOM_NUMBER = 100;
+const int SIZE_OF_ARRAY = 101;
 //  ==================================
 
 //  ===================
 //  Function Prototypes
 //  ===================
-    void FillArray(int[], int);
-    bool GoAgain();
-    void PrintArray(int[], int);
-    int  Random();
-    int  RandomAsk();
-    void SeedGenerator(unsigned int);
-    void SumofArray(int[], int);
+void FillArray(int[], int);
+bool GoAgain();
+void PrintArray(int[], int);
+int  Random();
+int  RandomAsk();
+void SeedGenerator(unsigned int);
+void SumofArray(int[], int);
 //  ============================
 
 //  ===============
 //  Function main()
 //  ===============
 //  Notes:
-//  1) This is the main function.
-//  =============================
-    int main() {
+//  This is the main function.
+//  =============
+int main() {
 
-        int numOfNums;
-        int dataArray[ROWS_IN_ARRAY][COLUMNS_IN_ARRAY];
-        
-        do {
-        SeedGenerator(1);
-        numOfNums = RandomAsk();
-        FillArray(dataArray, numOfNums);
+	int numOfNums;
+	int dataArray[SIZE_OF_ARRAY];
 
-        PrintArray(dataArray, numOfNums);
+	do {
+		SeedGenerator(1);
+		numOfNums = RandomAsk();
+		FillArray(dataArray, numOfNums);
 
-        SumofArray(dataArray, numOfNums);
-    
-        } while(GoAgain() == true); // do
+		PrintArray(dataArray, numOfNums);
 
-        return 0;
-    } // Function main()
+		SumofArray(dataArray, numOfNums);
+
+	} while (GoAgain() == true);
+
+	return 0;
+} // Function main()
 // =====================
 
 //  ===========
 //  FillArray()
 //  ===========
-    void FillArray(int arrayData[], int numOfNums) {
-        for (int ii = 0; ii < numOfNums; ii++) {
-            arrayData[ii] = Random();
-        }  // for
-    }  // FillArray()
+void FillArray(int arrayData[], int numOfNums) {
+	for (int ii = 0; ii < numOfNums; ii++) {
+		arrayData[ii] = Random();
+	}  // for
+}  // FillArray()
 //  =================
 
 //  ==================
@@ -69,34 +68,33 @@
 //     false if you want to discontinue.
 //  2) Requests user input.
 //  ================
-    bool GoAgain() {
+bool GoAgain() {
 
-        char verdict;
+	char verdict;
 
-            do {
-                cout << "Would you like to go again? (y/n) ==> ";
-                cin >> verdict;
-                cout << endl;
-                if (verdict == 'y') 
-                    return true;
-                else if (verdict == 'n')
-                cout << "Program Execution Terminating..." << endl << endl;
-                cout << "See you next time!" << endl;
-                    return false;
-            } while (verdict == 'y' || 'n'); // do
-    } // Function GoAgain()
+	do {
+		cout << "Would you like to go again? (y/n) ==> ";
+		cin >> verdict;
+		cout << endl;
+		if (verdict == 'y')
+			return true;
+		else if (verdict == 'n')
+			cout << "See you next time!" << endl;
+		return false;
+	} while (verdict == 'y' || 'n'); // do
+} // Function GoAgain()
 //  =======================
 
 //  ============
 //  PrintArray()
 //  ============
-    void PrintArray(int arrayData[], int numOfNums) {
+void PrintArray(int arrayData[], int numOfNums) {
 
-        cout << "The random numbers are" << endl;
-        for (int ii = 0; ii < numOfNums; ii++) {
-            cout << arrayData[ii] << " ";
-        }  // for
-    }  // PrintArray()
+	cout << "The random numbers are" << endl;
+	for (int ii = 0; ii < numOfNums; ii++) {
+		cout << arrayData[ii] << " ";
+	}  // for
+}  // PrintArray()
 //  ==================
 
 //  =================
@@ -106,53 +104,53 @@
 //  generated integers between
 //  zero and 2147483647.
 //  ====================
-    int Random() {
+int Random() {
 
-        int ranValue;
+	int ranValue;
 
-        ranValue = MIN_RANDOM_NUMBER +
-            rand() % (MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1);
+	ranValue = MIN_RANDOM_NUMBER +
+		rand() % (MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1);
 
-        return ranValue;
-    } // Function RandomNumber()
+	return ranValue;
+} // Function RandomNumber()
 // =============================
 
 //  ===========
 //  RandomAsk()
 //  ===========
-    int RandomAsk() {
+int RandomAsk() {
 
-        int num;
+	int num;
 
-        cout << "How many random numbers would you like ";
-        cin >> num;
-        cout << endl;
+	cout << "How many random numbers would you like ";
+	cin >> num;
+	cout << endl;
 
-        return num;
-    }  // RandomAsk()
+	return num;
+}  // RandomAsk()
 //  =================
 
 //  ===============
 //  SeedGenerator()
 //  ===============
-    void SeedGenerator(unsigned int seed) {
+void SeedGenerator(unsigned int seed) {
 
-        srand(seed);
+	srand(seed);
 
-    } // Function InitializeGenerator()
+} // Function InitializeGenerator()
 //  ===================================
 
 //  =====================
 //  Function SumofArray()
 //  =====================================
-    void SumofArray(int summedArrays[], int numOfNums) {
+void SumofArray(int summedArrays[], int numOfNums) {
 
-        int sum;
+	int sum;
 
-        sum = 0;
-        for (int ii = 0; ii < numOfNums; ii++) {
-            sum = sum + summedArrays[ii];
-        }  // for
-        cout << endl << endl << "Sum of the random numbers is " << sum << endl;
-    }  // Function SumofArray()
+	sum = 0;
+	for (int ii = 0; ii < numOfNums; ii++) {
+		sum = sum + summedArrays[ii];
+	}  // for
+	cout << endl << endl << "Sum of the random numbers is " << sum << endl;
+}  // Function SumofArray()
 //  ===========================
